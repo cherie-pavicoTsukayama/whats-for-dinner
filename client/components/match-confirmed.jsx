6fab663dd54450d91325246947b29fdb8a08d563
Logan Mulvihill <mulvihill.logan@gmail.com>
2020-05-05 12:11:30 -0700

    testing match logo sizing

--- client/components/match-confirmed.jsx
+++ client/components/match-confirmed.jsx
@@ -4,11 +4,11 @@ function Matchconfirmed() {
 
   return (
     <div className="container d-flex justify-content-center flex-column align-items-center one-hundred-view-height">
-      <div className="row w-100">
-        <div className="col-9">
-          <h1 className="display-4">Match!</h1>
+      <div className="row w-100 row-background">
+        <div className="col-8 column-background d-flex justify-content-end flex-column mt-4">
+          <h1 className="display-4 match-text match-background">Match!</h1>
         </div>
-        <div className="col-3"> Awesome!</div>
+        <div className="col-4 logo-space"></div>
       </div>
       <div className="row">
         <div className="col">
