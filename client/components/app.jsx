3c6405e81271ce443f158eecffb7591a79c84516
Logan Mulvihill <mulvihill.logan@gmail.com>
2020-05-04 22:37:17 -0700

    completed stateless rough draft

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import LandingPage from './landing-page';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -9,17 +9,19 @@ export default class App extends React.Component {
     };
   }
 
-  componentDidMount() {
-    fetch('/api/health-check')
-      .then(res => res.json())
-      .then(data => this.setState({ message: data.message || data.error }))
-      .catch(err => this.setState({ message: err.message }))
-      .finally(() => this.setState({ isLoading: false }));
-  }
+  // componentDidMount() {
+  //   fetch('/api/health-check')
+  //     .then(res => res.json())
+  //     .then(data => this.setState({ message: data.message || data.error }))
+  //     .catch(err => this.setState({ message: err.message }))
+  //     .finally(() => this.setState({ isLoading: false }));
+  // }
 
   render() {
-    return this.state.isLoading
-      ? <h1>Testing connections...</h1>
-      : <h1>{this.state.message.toUpperCase()}</h1>;
+    return (<LandingPage />);
   }
 }
+
+// return this.state.isLoading
+//   ? <h1>Testing connections...</h1>
+//   : <h1>{this.state.message.toUpperCase()}</h1>;
