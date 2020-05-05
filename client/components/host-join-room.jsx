9948cd9504a37ea06006efb5f2713e581dc1401e
Cherie <cherietsukayama@gmail.com>
2020-05-05 21:32:39 +0000

    Built out the interface of the front end of the host-can-join-room-front-end feature

--- client/components/host-join-room.jsx
+++ client/components/host-join-room.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
-export default class HostJoinRoom extends React.Component {
+export default function HostJoinRoom(props) {
 
-  render() {
-    return (
-      <div>hi</div>
-    );
-  }
+  return (
+    <div className={'host-join-room m-0 d-flex flex-wrap justify-content-center align-items-center'}>
+      <div className={'darken-background'}></div>
+      <div className= {'col-sm-12 m-0 p-0'}>
+        <div className={'col-sm-12 white-transparent-background pt-3 pb-3 mb-5'}>
+          <p className={'text-center montserrat-400 black-font-p  lead mb-0'}>Your Room <br /> With Room ID _________ <br /> Has Been Created </p>
+        </div>
+        <div className="col-sm-6 mb-5 d-flex flex-wrap justify-content-center">
+          <p className={'col-sm-12 text-center lead montserrat-400 grey-font'}><u>roomId auto populates</u></p>
+          <button type="button" className="btn btn-secondary grey-button m-3 lead">Join</button>
+        </div>
+      </div>
+
+    </div>
+  );
+
 }
