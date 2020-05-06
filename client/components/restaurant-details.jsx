07659c01e8e9b81beac75f17387c17b1fab00022
Cherie <cherietsukayama@gmail.com>
2020-05-06 23:32:27 +0000

    added dummy data to the app.jsx for the restaurants array, and using that data to render the name of the restaurant

--- client/components/restaurant-details.jsx
+++ client/components/restaurant-details.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 export default function RestaurantDetail(props) {
+
   return (
     <div>
-      hi
+      <div className="d-flex flex-wrap justify-content-center mt-4">
+        <h1 className="  montserrat-400 brand-main-color ">{props.restaurants[0].name}</h1>
+        <div></div>
+      </div>
     </div>
   );
 }
