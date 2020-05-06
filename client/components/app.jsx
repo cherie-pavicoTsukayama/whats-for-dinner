4196a7fb330664d92eaed51deddcdf97b6ec96bc
Logan Mulvihill <mulvihill.logan@gmail.com>
2020-05-05 22:34:41 -0700

    successfully tested rough draft modal

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,24 +1,51 @@
 import React from 'react';
-import Matchconfirmed from './match-confirmed';
+import MatchConfirmed from './match-confirmed';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       message: null,
-      isLoading: true
+      isLoading: true,
+      show: false
     };
+    this.showModal = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
   }
 
-  // componentDidMount() {
-  //   fetch('/api/health-check')
-  //     .then(res => res.json())
-  //     .then(data => this.setState({ message: data.message || data.error }))
-  //     .catch(err => this.setState({ message: err.message }))
-  //     .finally(() => this.setState({ isLoading: false }));
-  // }
+  showModal() {
+    this.setState({ show: true });
+  }
+
+  hideModal() {
+    this.setState({ show: false });
+  }
+
+  componentDidMount() {
+    fetch('/api/health-check')
+      .then(res => res.json())
+      .then(data => this.setState({ message: data.message || data.error }))
+      .catch(err => this.setState({ message: err.message }))
+      .finally(() => this.setState({ isLoading: false }));
+  }
 
   render() {
-    return (<Matchconfirmed />);
+    return (
+      <div>
+        <div className="app-test-picture one-hundred-view-height">
+          <button
+            type="button"
+            onClick={() => {
+              this.showModal();
+            }}
+            className="btn btn-secondary blue-rounded-button">
+            View Matches
+          </button>
+        </div >
+        <div>
+          <MatchConfirmed show={this.state.show} hideModal={this.hideModal} />
+        </div >
+      </div >
+    );
   }
 }
--- client/components/match-confirmed.jsx
+++ client/components/match-confirmed.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 
-function Matchconfirmed() {
+function MatchConfirmed(props) {
+
+  let className = null;
+  if (props.show === false) {
+    className = 'display-none';
+  } else {
+    className = 'display-modal';
+  }
 
   return (
-    <div className="container d-flex justify-content-center flex-column align-items-center one-hundred-view-height">
-      <div className="row w-100 row-background">
-        <div className="col-8 column-background d-flex justify-content-end flex-column mt-4">
-          <h1 className="display-4 match-text match-background">Match!</h1>
+
+    <div className={`match-modal container pb-5 justify-content-center flex-column align-items-center ${className}`}>
+      <div className="justify-content-center flex-column align-items-center match-modal-interior-container shadow">
+        <div className="row w-100">
+          <div className="col match-logo"></div>
         </div>
-        <div className="col-4 logo-space"></div>
-      </div>
-      <div className="row">
-        <div className="col">
-          <button type="button" className="btn btn-secondary blue-rounded-button m-3">View Matches</button>
+        <div className="row mt-3">
+          <div className="col secondary-div">
+            <button type="button" onClick={() => { props.hideModal(); }} className="btn btn-secondary blue-rounded-button">View Matches</button>
+          </div>
         </div>
       </div>
     </div>
   );
-}
 
-export default Matchconfirmed;
+}
+export default MatchConfirmed;
