b9dab502cec2ef347d0b57227f4b990610fc021f
Michael Zhu <mzhu23@gmail.com>
2020-05-07 08:47:07 +0000

    Changed UserJoinRoom component into a class

--- client/components/user-join-room.jsx
+++ client/components/user-join-room.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 
-export default function UserJoinRoom(props) {
-  return (
-    <div className={'user-join-room d-flex align-items-center'}>
-      <div className={'darken-background'}></div>
-      <form onSubmit={props.joinRoom} className={'col-sm-12 p-0 d-flex flex-column align-items-center'}>
-        <div className={'col-sm-12 white-transparent-background py-3 mb-5 d-flex justify-content-center'}>
-          <input className="user-join-input" type="text" placeholder="Entry Key" maxLength="8"/>
-        </div>
-        <button type="submit" className="btn btn-secondary grey-button m-3">Join Room</button>
-      </form>
-    </div>
-  );
+export default class UserJoinRoom extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className={'user-join-room d-flex align-items-center'}>
+        <div className={'darken-background'}></div>
+        <form className={'col-sm-12 p-0 d-flex flex-column align-items-center'}>
+          <div className={'col-sm-12 white-transparent-background py-3 mb-5 d-flex justify-content-center'}>
+            <input className="user-join-input" type="text" placeholder="Entry Key" maxLength="8" />
+          </div>
+          <button type="submit" className="btn btn-secondary grey-button m-3">Join Room</button>
+        </form>
+      </div>
+    );
+  }
 }
