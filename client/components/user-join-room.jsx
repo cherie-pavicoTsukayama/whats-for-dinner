d70fe8685908b09d8dcf8f4d32d69fb973714fda
Michael Zhu <mzhu23@gmail.com>
2020-05-07 09:00:10 +0000

    Defined handleChange method and added it to onChange handler of input so that state changes with new input

--- client/components/user-join-room.jsx
+++ client/components/user-join-room.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 export default class UserJoinRoom extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      input: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
   }
 
   render() {
@@ -12,7 +21,13 @@ export default class UserJoinRoom extends React.Component {
         <div className={'darken-background'}></div>
         <form className={'col-sm-12 p-0 d-flex flex-column align-items-center'}>
           <div className={'col-sm-12 white-transparent-background py-3 mb-5 d-flex justify-content-center'}>
-            <input className="user-join-input" type="text" placeholder="Entry Key" maxLength="8" />
+            <input
+              className="user-join-input"
+              value={this.state.input}
+              onChange={this.handleChange}
+              type="text"
+              placeholder="Entry Key"
+              maxLength="8"/>
           </div>
           <button type="submit" className="btn btn-secondary grey-button m-3">Join Room</button>
         </form>
