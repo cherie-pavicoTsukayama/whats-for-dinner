7f4229ce8b93f5e897f389829df30e70cd5c1529
Logan Mulvihill <mulvihill.logan@gmail.com>
2020-05-08 14:59:10 -0700

    tested stateful with data

--- client/components/voting-room.jsx
+++ client/components/voting-room.jsx
@@ -9,12 +9,51 @@ export default class VotingRoom extends React.Component {
       isLoading: true,
       view: 'restaurantRoom',
       match: true,
-      currentImageIndex: 0
+      currentImageIndex: 0,
+      details: null
     };
     this.showModal = this.showModal.bind(this);
     this.hideModal = this.hideModal.bind(this);
-    this.handleClickNext = this.handleClickNext.bind(this);
-    this.handleClickBack = this.handleClickBack.bind(this);
+    this.handleClickNextImage = this.handleClickNextImage.bind(this);
+    this.handleClickBackImage = this.handleClickBackImage.bind(this);
+    this.renderStarRating = this.renderStarRating.bind(this);
+  }
+
+  renderStarRating() {
+    const rating = this.props.restaurant.rating;
+    switch (rating) {
+      case 1:
+        return <img className="img-fluid review-star-image pb-1" src="./images/1-stars.png" alt="" />;
+      case 1.5:
+        return <img className="img-fluid review-star-image pb-1" src="./images/1-and-a-half-stars.png" alt="" />;
+      case 2:
+        return <img className="img-fluid review-star-image pb-1" src="./images/2-stars.png" alt="" />;
+      case 2.5:
+        return <img className="img-fluid review-star-image pb-1" src="./images/2-and-a-half-stars.png" alt="" />;
+      case 3:
+        return <img className="img-fluid review-star-image pb-1" src="./images/3-stars.png" alt="" />;
+      case 3.5:
+        return <img className="img-fluid review-star-image pb-1" src="./images/3-and-a-half-stars.png" alt="" />;
+      case 4:
+        return <img className="img-fluid review-star-image pb-1" src="./images/4-stars.png" alt="" />;
+      case 4.5:
+        return <img className="img-fluid review-star-image pb-1" src="./images/4-and-a-half-stars.png" alt="" />;
+      case 5:
+        return <img className="img-fluid review-star-image pb-1" src="./images/5-stars.png" alt="" />;
+      default:
+        break;
+    }
+  }
+
+  getDetails() {
+    fetch(`/api/restaurants/${this.props.restaurant.id}`)
+      .then(result => result.json())
+      .then(data => {
+        this.setState({
+          details: data
+        });
+      })
+      .catch(err => console.error(err));
   }
 
   showModal() {
@@ -25,7 +64,7 @@ export default class VotingRoom extends React.Component {
     this.setState({ match: false });
   }
 
-  handleClickNext() {
+  handleClickNextImage() {
     const currentImageIndex = this.state.currentImageIndex;
     let newImageIndex = null;
     if (currentImageIndex === this.props.images.length - 1) {
@@ -38,7 +77,7 @@ export default class VotingRoom extends React.Component {
     });
   }
 
-  handleClickBack() {
+  handleClickBackImage() {
     const currentImageIndex = this.state.currentImageIndex;
     let newImageIndex = null;
     if (currentImageIndex === 0) {
@@ -52,43 +91,40 @@ export default class VotingRoom extends React.Component {
   }
 
   render() {
+
     return (
       <div className={'container-fluid d-flex flex-column restaurant-room min-vh-100 min-vw-100  pl-0 pr-0'}>
         <div className={'col-sm pl-2 pr-0 mt-3'}>
           <button type="button" className="btn btn-secondary leave-room-button shadow view-height-four">Leave Room</button>
         </div>
-
-        <div className={'col d-flex justify-content-center align-items-center flex-column pl-0 pr-0 mb-4'}>
-          <div className={' restaurant-title '}>Restaurant Name</div>
+        <div className={'col d-flex justify-content-center align-items-center flex-column pl-0 pr-0 mb-2'}>
+          <div className={'restaurant-title'}>{this.props.restaurant.name}</div>
           <div className={'mt-2'}>
-            <img src={'/images/1-start.png'} className={'img-fluid review-star-image pb-1'} alt="Yelp Star Review Rating" />
+            {this.renderStarRating()}
           </div>
         </div>
-
         <div className={'col d-flex flex-wrap justify-content-center  pl-0 pr-0 match-image-container'}>
-          <div className={'col pl-0 pr-0 view-height-fifty'}>
+          <div className={'col pl-0 pr-0 view-height-forty-five'}>
             <img src={this.props.images[this.state.currentImageIndex]} alt="Yelp Restaurant Business Image" className={' h-100 w-100 '} />
           </div>
           <div className={'match-button-container d-flex justify-content-between align-items-center h-100 w-100'}>
             <button className='btn'>
-              <i className={'fas fa-chevron-left fa-4x food-choice-arrow'} onClick={this.handleClickBack}></i>
+              <i className={'fas fa-chevron-left fa-4x food-choice-arrow'} onClick={this.handleClickBackImage}></i>
             </button>
             <button className={'btn'}>
-              <i className={'fas fa-chevron-right fa-4x food-choice-arrow'} onClick={this.handleClickNext}></i>
+              <i className={'fas fa-chevron-right fa-4x food-choice-arrow'} onClick={this.handleClickNextImage}></i>
             </button>
           </div>
         </div>
-
         <div className={'col-sm d-flex justify-content-center  pl-0 pr-0'}>
           <button type="button" className="btn btn-secondary grey-button m-3 shadow-sm">Info</button>
         </div>
-
-        <div className={'col d-flex justify-content-around brand-blue  pl-0 pr-0'}>
-          <button className='btn'>
-            <i className={'fas fa-caret-left white fa-5x'} onClick={this.handleClickBack}></i>
+        <div className={'col d-flex justify-content-center brand-blue  pl-0 pr-0 restaurant-button-choice'}>
+          <button className='btn '>
+            <i className={'fas fa-caret-left white fa-5x'} onClick={() => { this.props.decrementRestaurant(); }} ></i>
           </button>
           <button className={'btn'}>
-            <i className={'fas fa-caret-right white fa-5x'} onClick={this.handleClickNext}></i>
+            <i className={'fas fa-caret-right white fa-5x'} onClick={() => { this.props.incrementRestaurant(); }}></i>
           </button>
         </div>
 
