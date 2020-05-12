e8636b71015c2e50b460e81485497096470353e5
Cherie <cherietsukayama@gmail.com>
2020-05-12 15:57:27 +0000

    adjusted the styling for the room closed modal, added the match icon to it.

--- client/components/room-closed-modal.jsx
+++ client/components/room-closed-modal.jsx
@@ -10,14 +10,15 @@ function RoomClosedModal(props) {
 
   return (
 
-    <div className={`match-modal container pb-5  ${className}`}>
-      <div className=" d-flex justify-content-center flex-column align-items-center match-modal-interior-container shadow">
-        <img src="" alt=""/>
+    <div className={`match-modal container ${className}`}>
+      <div className=" pb-4 d-flex justify-content-center flex-column align-content-around match-modal-interior-container shadow">
+        <div className="w-100 match-icon">
+        </div>
         <p className="text-center montserrat-500 lead brand-blue-text">Sorry this room <br/> has been closed</p>
         <button
           type="button"
           onClick={props.hideRoomClosedModal}
-          className="btn orange-rounded-button shadow-sm">
+          className="btn orange-rounded-button shadow-sm align-self-center">
               Ok
         </button>
       </div>
