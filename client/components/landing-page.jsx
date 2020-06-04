77c222bc94064ffe6f2d56487c5d0c198694d27b
Cherie <cherietsukayama@gmail.com>
2020-06-04 03:13:29 +0000

    added the directions to the landing page. Made it responsive for iphone 678 portrait & landscape, iphone x portrait & landscape, ipad portrait & landscape and desktop.

--- client/components/landing-page.jsx
+++ client/components/landing-page.jsx
@@ -9,28 +9,28 @@ export default function LandingPage(props) {
           <div className="landing-page-logo"></div>
         </div>
       </div>
-      <div className="my-2 d-flex flex-wrap align-content-between landing-page-layout">
-        <div className="d-flex flex-nowrap justify-content-around mt-3">
+      <div className="my-2 d-flex flex-wrap align-content-between justify-content-center landing-page-layout">
+        <div className="col-md-8 col-lg-5 d-flex flex-nowrap justify-content-around mt-3">
           <div className=" p-1 py-2 mx-1 brand-blue-text montserrat-500 instruction-container">
             <h5 className="text-center">1</h5>
             <p className="m-0 text-center instruction-text">
-              Make a room <br></br> and share <br></br> your entry key with a friend.
+              Make a room  and share your entry key with a friend.
             </p>
           </div>
           <div className=" p-1 py-2 mx-1 brand-blue-text montserrat-500 instruction-container">
             <h5 className="text-center">2</h5>
             <p className="m-0 text-center instruction-text">
-              Already have an entry key? <br></br> Join your <br></br>friend&apos;s room!
+              Already have an entry key? Join your friend&apos;s room!
             </p>
           </div>
           <div className=" p-1 m py-2 mx-1 brand-blue-text montserrat-500 instruction-container">
             <h5 className="text-center">3</h5>
             <p className="m-0 text-center instruction-text">
-              Start liking <br></br>restaurants<br></br>and match<br></br>with a friend!
+              Start liking restaurant and match with a friend!
             </p>
           </div>
         </div>
-        <div className="d-flex flex-wrap justify-content-center mt-auto">
+        <div className="col-12 d-flex flex-wrap justify-content-center mt-auto">
           <div>
             <button
               type="button"
