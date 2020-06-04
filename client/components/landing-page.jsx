d2089da1c96bfe21b2b8e76f6c715be615996189
Cherie <cherietsukayama@gmail.com>
2020-06-04 01:54:30 +0000

    added and styled the directions on the landing page.

--- client/components/landing-page.jsx
+++ client/components/landing-page.jsx
@@ -7,6 +7,26 @@ export default function LandingPage(props) {
       <div className="transparent-white-bg d-flex justify-content-center shadow">
         <div className="landing-page-logo"></div>
       </div>
+      <div className="d-flex flex-nowrap justify-content-around">
+        <div className=" p-1 py-3 mx-1 brand-blue-text montserrat-500 instruction-container">
+          <h5 className="text-center">1</h5>
+          <p className="m-0 text-center instruction-text">
+            Make a room and share your entry key with a friend.
+          </p>
+        </div>
+        <div className=" p-1 py-3 mx-1 brand-blue-text montserrat-500 instruction-container">
+          <h5 className="text-center">1</h5>
+          <p className="m-0 text-center instruction-text">
+            Make a room and share your entry key with a friend.
+          </p>
+        </div>
+        <div className=" p-1 m py-3 mx-1 brand-blue-text montserrat-500 instruction-container">
+          <h5 className="text-center">1</h5>
+          <p className="m-0 text-center instruction-text">
+            Make a room and share your entry key with a friend.
+          </p>
+        </div>
+      </div>
       <button
         type="button"
         className="btn btn-secondary white-button m-3 shadow"
