ad2374a7b4ee7200c97ae1972604edd10fc2d084
Cherie <cherietsukayama@gmail.com>
2020-06-08 19:47:24 +0000

    modified the close room modal on the iphone 678 landscape view

--- client/components/voting-room.jsx
+++ client/components/voting-room.jsx
@@ -18,7 +18,7 @@ export default class VotingRoom extends React.Component {
       isLeaving: false,
       restaurantId: null,
       isRoomClosedIntervalId: null,
-      isRoomClosed: null
+      isRoomClosed: false
     };
     this.isRoomClosedIntervalId = null;
     this.showModal = this.showModal.bind(this);
